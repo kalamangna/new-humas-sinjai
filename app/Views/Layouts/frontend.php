0bfed61576937be31111c89d8d23c6a6067b827b
Abd Dzuljalali Wal Ikram <35287914+kalamangna@users.noreply.github.com>
2026-02-12 11:22:11 +0800

    style: further decrease top padding for tighter layout

--- app/Views/Layouts/frontend.php
+++ app/Views/Layouts/frontend.php
@@ -195,7 +195,7 @@
 
     <!-- Main Content -->
     <main class="min-h-screen">
-        <div class="<?= url_is('/') ? '' : 'pb-12 md:pb-20 pt-4 md:pt-8' ?>">
+        <div class="<?= url_is('/') ? '' : 'pb-12 md:pb-20 pt-2 md:pt-4' ?>">
             <?= $this->renderSection('content') ?>
         </div>
     </main>

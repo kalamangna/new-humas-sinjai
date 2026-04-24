9081d5d8e1a9489140a54db9c33b9853b39bd098
Abd Dzuljalali Wal Ikram <35287914+kalamangna@users.noreply.github.com>
2026-04-24 10:05:43 +0800

    chore: cleanup temporary test files

--- test_api.php
+++
@@ -1,6 +0,0 @@
-<?php
-$client = \Config\Services::curlrequest();
-$response = $client->get('http://apps.sinjaikab.go.id/api/pegawai/get_wilayah', [
-    'query' => ['tipe' => 'Kelurahan']
-]);
-echo substr($response->getBody(), 0, 100);
